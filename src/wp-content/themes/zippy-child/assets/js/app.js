1558c46a7e6959d1c48b8b7f89331762c2346bbe
shin <nhathanh024@gmail.com>
2026-02-10 18:25:37 +0700

    init code

---
+++ src/wp-content/themes/zippy-child/assets/js/app.js
@@ -0,0 +1,27 @@
+/* This code below for JavaScript ES6 */
+
+/*
+import { DisplayLabel } from './components/DisplayLabel';
+
+let Main = {
+  init: async function () {
+
+    // initialize demo javascript component - async/await invokes some
+    //  level of babel transformation
+    const displayLabel = new DisplayLabel();
+    await displayLabel.init();
+
+  }
+};
+
+Main.init();
+
+*/
+
+/* Do not remove this code if use Jquery */
+
+"use strict";
+$ = jQuery;
+$(document).ready(function () {
+  console.log($(".admin"));
+});
