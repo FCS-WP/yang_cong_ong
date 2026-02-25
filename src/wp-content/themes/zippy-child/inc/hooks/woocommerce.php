53fe956bee95b8caa31e8f532ffbd60336e1b699
Lucass1611 <chuong.nguyenquoc@floatingcube.com>
2026-02-25 17:28:49 +0700

    Update code

--- src/wp-content/themes/zippy-child/inc/hooks/woocommerce.php
+++ src/wp-content/themes/zippy-child/inc/hooks/woocommerce.php
@@ -28,3 +28,53 @@ function custom_flatsome_quantity()
 
     echo '</form>';
 }
+
+// Create shortcode breadcrumb for page
+
+add_shortcode('wc_page_breadcrumb', function ($atts) {
+
+    // Woo context -> dùng Woo breadcrumb
+    if (function_exists('woocommerce_breadcrumb') && function_exists('is_woocommerce')) {
+        if (is_woocommerce() || is_cart() || is_checkout() || is_account_page()) {
+            ob_start();
+            woocommerce_breadcrumb([
+                'wrap_before' => '<nav class="woocommerce-breadcrumb" aria-label="Breadcrumb">',
+                'wrap_after'  => '</nav>',
+                'delimiter'   => '&nbsp;&#47;&nbsp;',
+            ]);
+            return ob_get_clean();
+        }
+    }
+
+    if (is_page()) {
+        global $post;
+
+        $delimiter = '&nbsp;&#47;&nbsp;';
+        $home_text = 'Home';
+
+        $crumbs = [];
+        $crumbs[] = '<a href="' . esc_url(home_url('/')) . '">' . esc_html($home_text) . '</a>';
+
+        $parents = [];
+        $parent_id = (int) $post->post_parent;
+
+        while ($parent_id) {
+            $p = get_post($parent_id);
+            if (! $p) break;
+            $parents[] = $p;
+            $parent_id = (int) $p->post_parent;
+        }
+
+        $parents = array_reverse($parents);
+
+        foreach ($parents as $p) {
+            $crumbs[] = '<a href="' . esc_url(get_permalink($p->ID)) . '">' . esc_html(get_the_title($p->ID)) . '</a>';
+        }
+
+        $crumbs[] = '<span>' . esc_html(get_the_title($post->ID)) . '</span>';
+
+        return '<nav class="woocommerce-breadcrumb" aria-label="Breadcrumb">' . implode($delimiter, $crumbs) . '</nav>';
+    }
+
+    return '';
+});
